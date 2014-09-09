90a1e91255c19b6d8a49f8936517e3a3608e4bee
Yvette <yvettecook@gmail.com>
2014-09-09 16:18:51 +0100

    Collects GitHub usernames

--- user_input.rb
+++ user_input.rb
@@ -8,7 +8,9 @@ def input_students
 	# while the name is not empty, repeat this code
 	while !name.empty? do
 		# add the student has to the array
-		students << {:name => name, :cohort => :september}
+		puts "What is their GitHub username?"
+		git = gets.chomp
+		students << {:name => name, :cohort => :september, :git => git}
 		puts "Now we have #{students.length} students"
 		# get another name from the user
 		name = gets.chomp
@@ -23,12 +25,28 @@ def print_header
 end
 
 def print_out(students)
-	students.each_with_index { |student, index| print "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)\n" }
+	students.each_with_index { |student, index| print "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort) Git: #{student[:git]}\n" }
 end
 
 def print_footer(students)
 	print "-------------\n"
 	print "Overall, we have #{students.length} great students\n"
+end
+
+def print_a(students)
+	students.each { |student| 
+		if student[:name].start_with?('A')
+			puts "#{student[:name]}"
+		end	
+	}
+end
+
+def print_less_than_12(students)
+	students.each { |student| 
+		if student[:name].length < 12
+			puts "#{student[:name]}"
+		end
+	}
 end
 
 students = input_students
